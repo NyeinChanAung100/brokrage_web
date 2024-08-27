e39804c791685d757ff118c75df5c70decfffc12
La Pyae Htun <lapyaehtun369@gmail.com>
2024-08-27 14:57:49 +0630

    buyphp

--- server/market/buy.php
+++ server/market/buy.php
@@ -1,5 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
 
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $data = json_decode(file_get_contents("php://input"), true);
+}   
 
 ?>

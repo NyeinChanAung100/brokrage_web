93a52f47a4f70ec3d2c5f3e614da625347b2f0bb
La Pyae Htun <lapyaehtun369@gmail.com>
2024-08-30 12:56:16 +0630

    qsd

--- server/main.sql
+++ server/main.sql
@@ -36,7 +36,7 @@ CREATE TABLE market_cap (
     FOREIGN KEY (item_id) REFERENCES items(id)
 );
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
